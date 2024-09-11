7bdbcfabef4911ca2766558842eca13ebc8f6bfe
Daniel Kim <151947516+danielminjaekim@users.noreply.github.com>
2024-09-11 13:38:54 -0600

    Completed "Week-2" Assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return(
+        <div>
+            <p>Daniel Kim</p>
+            <p>
+                <Link href="https://github.com/danielminjaekim/cprg306-assignments">My GitHub Repository</Link>
+            </p>
+        </div>
+    );
+}
